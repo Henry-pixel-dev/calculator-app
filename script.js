66983bf151d09752fe2a2da73430b8b2a97a0954
Henry-pixel-dev <henrylaskid5@gmail.com>
2026-02-12 00:12:42 +0100

    update script.js

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ let currentTheme = 1;
 scrollBg.addEventListener('click', () => {
     console.log('clicked')
     if (currentTheme === 1){
-        scrollBall.classList.remove('scroll1', 'scroll2', 'scroll3');  // Remove all positions
+        scrollBall.classList.remove('scroll1', 'scroll2', 'scroll3');  
         scrollBall.classList.add('scroll2')
         document.body.classList.remove('theme-purple', 'theme-light');
         document.body.classList.add('theme-light');
         currentTheme = 2
     } else if (currentTheme === 2){
-        scrollBall.classList.remove('scroll1', 'scroll2', 'scroll3');  // Remove all positions
+        scrollBall.classList.remove('scroll1', 'scroll2', 'scroll3');  
         scrollBall.classList.add('scroll3')
         
         document.body.classList.remove('theme-light', 'theme-purple');
         document.body.classList.add('theme-purple');
         currentTheme = 3
     } else {
-        scrollBall.classList.remove('scroll1', 'scroll2', 'scroll3');  // Remove all positions
+        scrollBall.classList.remove('scroll1', 'scroll2', 'scroll3'); 
         scrollBall.classList.add('scroll1')
         document.body.classList.remove('theme-light', 'theme-purple');
         currentTheme = 1
